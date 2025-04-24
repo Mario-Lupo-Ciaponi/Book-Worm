3412e2aa3ff1ed11bd20d4a1754f89118eb0e474
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2025-04-24 13:01:04 +0300

    Added some GUI, but with no add book functionality.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from sqlalchemy import insert, select
 from sqlalchemy.orm import sessionmaker
+import customtkinter as ctk
 
 from models import engine, Book
 
@@ -38,65 +39,128 @@ class Repo:
 
         return result.scalars().all()
 
+class BookWormApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-def instruction():
-    print("Actions:\n1) Add book;\n2) See added books\n3) Quit\n")
+        self.title("Book Worm")
+        self.geometry("500x500")
+
+        self.columnconfigure((0, 1, 2), weight=1)
+
+
+        # Label that will show up at the top
+        self.book_worm_label = ctk.CTkLabel(
+            self,
+            text="BookWorm - Your Personal Library",
+            font=("Helvetica", 20, "bold")
+        )
+        self.book_worm_label.grid(
+            row=0,
+            column=0,
+            columnspan=3,
+            sticky="n",
+            pady=(20, 10))
+
+        self.label_for_search = ctk.CTkLabel(
+            self,
+            text="Search:"
+        )
+        self.label_for_search.grid(
+            row=1,
+            column=0,
+            padx=10,
+            pady=10,
+            sticky="e"
+        )
+
+        self.search_entry = ctk.CTkEntry(
+            self,
+        )
+        self.search_entry.grid(
+            row=1,
+            column=1,
+            padx=10,
+            pady=10,
+            sticky="we"
+        )
+
+        self.button_for_search = ctk.CTkButton(
+            self,
+            text="Search"
+        )
+        self.button_for_search.grid(
+            row=1,
+            column=2,
+            padx=10,
+            pady=10,
+            sticky="w"
+        )
+
+        self.add_book_button = ctk.CTkButton(
+            self,
+            text="+ Add book",
+            width=100
+        )
+        self.add_book_button.grid(
+            row=2,
+            column=2,
+            padx=20,
+            pady=10,
+        )
+
+        self.label_for_booklist = ctk.CTkLabel(
+            self,
+            text="Booklist:",
+            font=("Helvetica", 17, "bold")
+        )
+        self.label_for_booklist.grid(
+            row=3,
+            column=0,
+            columnspan=3,
+            sticky="n",
+            pady=(20, 5)
+        )
+
+        self.scrollable_frame_books = ctk.CTkScrollableFrame(
+            self,
+            width=400,
+        )
+        self.scrollable_frame_books.grid(
+            row=4,
+            column=0,
+            columnspan=3,
+            sticky="n",
+            pady=(20, 5)
+        )
+
+        self.add_books_to_scrollable_frame()
+
+    def add_books_to_scrollable_frame(self):
+        for book in self.scrollable_frame_books.winfo_children():
+            book.destroy()
+
+        with Session() as session:
+            repo = Repo(session)
+
+            books = repo.get_all_books()
+
+            for book in books:
+                book_for_frame = ctk.CTkLabel(
+                    self.scrollable_frame_books,
+                    text=f"Title: {book.title};\n"
+                         f"Author: {book.author};\n"
+                         f"Genre: {book.genre};\n"
+                         f"Year: {book.year};\n"
+                         f"ISBN: {book.isbn}\n",
+                )
+                book_for_frame.pack()
+
+
 
 def main():
-    print("Hello and welcome!\n")
-
-    instruction()
-
-    while True:
-        try:
-            option = int(input("Your option(e.g. '1', '2' and ect.): "))
-        except ValueError:
-            print("The input must be an input!")
-        else:
-            if 0 > option or option > 3:
-                print("The input must be between 1 and 3!")
-                continue
-            if option == 1:
-                print()
-
-                try:
-                    title = input("Title: ")
-                    author = input("Author: ")
-                    genre = input("Genre: ")
-                    year = input("Year(integer): ")#None,
-                    isbn = input("ISBN: ") #None,
-                    description = input("Description: ") #None
-
-                    year = int(year) if year else None
-                    isbn = isbn if isbn else None
-                    description = description if description else None
-                except ValueError:
-                    print("Year must be an integer number!")
-                    continue
-                else:
-                    with Session() as session:
-                        repo = Repo(session)
-
-                        repo.add_book(title, author, genre, year, isbn, description)
-                        print(f"'{title}' added successfully!")
-            elif option == 2:
-                print()
-
-                with Session() as session:
-                    repo = Repo(session)
-
-                    books = repo.get_all_books()
-
-                    for b in books:
-                        print(f"Title: {b.title}\n"
-                              f"Author: {b.author}\n"
-                              f"Genre: {b.genre}\n"
-                              f"Year: {b.year if b.year else 'no year'}\n"
-                              f"ISBN: {b.isbn if b.isbn else 'no ISBN'}\n"
-                              f"Description: {b.description if b.description else 'no description'}\n")
-            else:
-                break
-
+    book_worm_app = BookWormApp()
+    book_worm_app.mainloop()
 
 
 if __name__ == "__main__":
